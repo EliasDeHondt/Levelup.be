8e6acf35f3c8ed0dc18cf51c38dfdc1ca745d16c
EliasDeHondt <Elias.dehondt@outlook.com>
2022-12-17 09:51:33 +0100

    Version 0.3

---
+++ assets/js/right-click-menu.js
@@ -0,0 +1,39 @@
+/*
+ * Van Elias De Hondt
+ * https://eliasdh.com
+ */
+/* Right Click Menu */
+function hideMenu() {
+    $('#contextMenu').hide();
+    sessionStorage.removeItem('select');
+}
+
+$( document ).ready(function() {
+    
+    $( document ).contextmenu(function(e) {
+      e.preventDefault();
+      localStorage.setItem('select',document.getSelection());
+
+      hideMenu();
+
+      let menu = document.getElementById("contextMenu")
+      menu.style.display = 'block';
+      menu.style.left = e.pageX + "px";
+      menu.style.top = e.pageY + "px";
+    });
+
+    $( document ).on('click', hideMenu);
+});
+/* Right Click Menu */
+
+/* Copy Link Address */
+function copylinkaddress() {
+  navigator.clipboard.writeText(window.location.href);
+}
+/* Copy Link Address */
+
+/* Copy To Clipboard */
+function copytoclip() {
+  navigator.clipboard.writeText(localStorage.getItem('select'));
+}
+/* Copy To Clipboard */
---
+++ assets/js/script-1.js
@@ -0,0 +1,36 @@
+/*
+ * Van Elias De Hondt
+ * https://eliasdh.com
+ */
+function loadHTML(myDivId, url) {
+    var xmlhttp;
+    if (window.XMLHttpRequest)
+    {
+        xmlhttp = new XMLHttpRequest();
+    }
+    else
+    {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function()
+    {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE )
+        {
+           if(xmlhttp.status == 200){
+               document.getElementById(myDivId).innerHTML = xmlhttp.responseText;
+               var allScripts = document.getElementById(myDivId).getElementsByTagName('script-1');
+               for (const element of allScripts)
+               {
+                   eval(element.innerHTML)
+               }
+           }
+           else {
+               alert('Error');
+           }
+        }
+    }
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
